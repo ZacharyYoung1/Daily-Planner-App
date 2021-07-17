2a4164a294ca7f167c2b1c7f3eaf97ce35893fe0
Zach Young <Zyoung.young9@gmail.com>
2021-07-16 22:15:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,207 @@
+var date = new Date();
+document.getElementById("currentDay").innerHTML = date;
+
+var storageInput1 = document.querySelector('.storage');
+var button1 = document.querySelector('.button');
+var storedInput1= localStorage.getItem('textinput1')
+
+if(storageInput1) {
+storageInput1.textContent = storedInput1
+
+}
+
+storageInput1.addEventListener('input', letter => {
+storageInput1.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput1', storageInput1.textContent)
+
+}
+button1.addEventListener('click', saveLocal)
+
+var storageInput2 = document.querySelector('.storage2');
+var button2 = document.querySelector('.button2');
+var storedInput2= localStorage.getItem('textinput2')
+
+if(storageInput2) {
+storageInput2.textContent = storedInput2
+
+}
+
+storageInput2.addEventListener('input', letter => {
+storageInput2.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput2', storageInput2.textContent)
+
+}
+button2.addEventListener('click', saveLocal)
+
+
+
+var storageInput3 = document.querySelector('.storage3');
+var button3 = document.querySelector('.button3');
+var storedInput3= localStorage.getItem('textinput3')
+
+if(storageInput3) {
+storageInput3.textContent = storedInput3
+
+}
+
+storageInput3.addEventListener('input', letter => {
+storageInput3.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput3', storageInput3.textContent)
+
+}
+button3.addEventListener('click', saveLocal)
+
+
+
+var storageInput4 = document.querySelector('.storage4');
+var button4 = document.querySelector('.button4');
+var storedInput4= localStorage.getItem('textinput4')
+
+if(storageInput4) {
+storageInput4.textContent = storedInput4
+
+}
+
+storageInput4.addEventListener('input', letter => {
+storageInput4.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput4', storageInput4.textContent)
+
+}
+button4.addEventListener('click', saveLocal)
+
+var storageInput5 = document.querySelector('.storage5');
+var button5 = document.querySelector('.button5');
+var storedInput5= localStorage.getItem('textinput5')
+
+if(storageInput5) {
+storageInput5.textContent = storedInput5
+
+}
+
+storageInput5.addEventListener('input', letter => {
+storageInput5.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput5', storageInput5.textContent)
+
+}
+button5.addEventListener('click', saveLocal)
+
+
+var storageInput6 = document.querySelector('.storage6');
+var button6 = document.querySelector('.button6');
+var storedInput6= localStorage.getItem('textinput6')
+
+if(storageInput6) {
+storageInput6.textContent = storedInput6
+
+}
+
+storageInput6.addEventListener('input', letter => {
+storageInput6.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput6', storageInput6.textContent)
+
+}
+button6.addEventListener('click', saveLocal)
+
+var storageInput7 = document.querySelector('.storage7');
+var button7 = document.querySelector('.button7');
+var storedInput7= localStorage.getItem('textinput7')
+
+if(storageInput7) {
+storageInput7.textContent = storedInput7
+
+}
+
+storageInput7.addEventListener('input', letter => {
+storageInput7.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput7', storageInput7.textContent)
+
+}
+button7.addEventListener('click', saveLocal)
+
+var storageInput8 = document.querySelector('.storage8');
+var button8 = document.querySelector('.button8');
+var storedInput8= localStorage.getItem('textinput8')
+
+if(storageInput8) {
+storageInput8.textContent = storedInput8
+
+}
+
+storageInput8.addEventListener('input', letter => {
+storageInput8.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput8', storageInput8.textContent)
+
+}
+button8.addEventListener('click', saveLocal)
+
+var storageInput9 = document.querySelector('.storage9');
+var button9 = document.querySelector('.button9');
+var storedInput9= localStorage.getItem('textinput9')
+
+if(storageInput9) {
+storageInput9.textContent = storedInput9
+
+}
+
+storageInput9.addEventListener('input', letter => {
+storageInput9.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput9', storageInput9.textContent)
+
+}
+button9.addEventListener('click', saveLocal)
+
+var storageInput10 = document.querySelector('.storage10');
+var button10 = document.querySelector('.button10');
+var storedInput10= localStorage.getItem('textinput10')
+
+if(storageInput10) {
+storageInput10.textContent = storedInput10
+
+}
+
+storageInput10.addEventListener('input', letter => {
+storageInput10.textContent = letter.target.value
+
+});
+
+var saveLocal = () => {
+localStorage.setItem('textinput10', storageInput10.textContent)
+
+}
+button10.addEventListener('click', saveLocal)
